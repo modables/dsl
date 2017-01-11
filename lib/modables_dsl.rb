e771aa6dd3b1427c25735e0e471c90eed894adb4
chefcc <chefcc@modabl.es>
2017-01-11 07:33:25 -0500

    Move executable to bin.

--- lib/modables_dsl.rb
+++ lib/modables_dsl.rb
@@ -1,9 +1,7 @@
+$:.unshift File.join File.dirname(__FILE__), 'modables_dsl'
+
 require 'active_support/json'
 require 'active_support/core_ext/hash/deep_merge'
 
-require_relative 'modables_dsl/dsl/arguments'
-require_relative 'modables_dsl/dsl/core'
-
-template = ModablesDSL::DSL.instance_eval(File.read('main.tf.rb'))
-
-print template
+require 'modables_dsl/dsl/arguments'
+require 'modables_dsl/dsl/core'
